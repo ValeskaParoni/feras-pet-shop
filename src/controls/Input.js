cf79cfed0f2ee477b7652b653e5745ea7bc3b371
kenchen12 <kenchen12@gmail.com>
2018-05-17 16:25:17 -0300

    fixed input

--- src/controls/Input.js
+++ src/controls/Input.js
@@ -9,13 +9,28 @@ props:
 class Input extends React.Component{
   constructor (props, context){
     super(props, context);
+    this.state = {value: ''};
+    this.handleChange = this.handleChange.bind(this);
+    this.getValue = this.getValue.bind(this);
   }
-
+  getValue(){
+    return this.state.value;
+  }
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
   render(){
-    return (
-      <input type={this.props.type} name={this.props.name} id={this.props.id} value={this.props.value} min={this.props.min} 
-      step={this.props.step}/>
-    );
+    if (this.props.textArea == 'true') {
+      return (
+        <textarea name={this.props.name} rows={this.props.rows} cols={this.props.cols} value={this.state.value} onChange={this.handleChange} />
+      );
+    }
+    else{
+      return (
+        <input type={this.props.type} name={this.props.name} id={this.props.id} value={this.state.value} min={this.props.min}
+        step={this.props.step} onChange={this.handleChange}/>
+      );
+    }
   }
 }
 
